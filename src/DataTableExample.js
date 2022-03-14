411e47f082cd3a972558713cd4c00667daf9b60f
Milton Navarro <mnavarro@gmail.com>
2022-03-14 15:07:39 -0600

    Export datagrid to excel and pdf

--- src/DataTableExample.js
+++ src/DataTableExample.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import axios from "axios";
+import { Button } from "primereact/button";
+import jsPDF from "jspdf";
 
 const DataTableExample = () => {
   const [posts, setPosts] = useState([]);
@@ -12,6 +14,74 @@ const DataTableExample = () => {
       .then((res) => setPosts(res.data));
   }, []);
 
+  const cols = [
+    { field: "userId", header: "userId" },
+    { field: "id", header: "id" },
+    { field: "title", header: "title" },
+    { field: "body", header: "body" },
+  ];
+
+  const exportColumns = cols.map((col) => ({
+    title: col.header,
+    dataKey: col.field,
+  }));
+
+  const exportExcel = () => {
+    import("xlsx").then((xlsx) => {
+      const workSheet = xlsx.utils.json_to_sheet(posts);
+      const workBook = { Sheets: { data: workSheet }, SheetNames: ["data"] };
+      const excelBuffer = xlsx.write(workBook, {
+        bookType: "xlsx",
+        type: "array",
+      });
+      saveAsExcelFile(excelBuffer, "posts");
+    });
+  };
+
+  const saveAsExcelFile = (buffer, fileName) => {
+    import("file-saver").then((FileSaver) => {
+      let EXCEL_TYPE =
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+      let EXCEL_EXTENSION = ".xlsx";
+      const data = new Blob([buffer], {
+        type: EXCEL_TYPE,
+      });
+      FileSaver.saveAs(
+        data,
+        fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
+      );
+    });
+  };
+
+  const exportPDF = () => {
+    import("jspdf").then((jsPDF) => {
+      import("jspdf-autotable").then(() => {
+        const doc = new jsPDF.default(0, 0);
+        doc.autoTable(exportColumns, posts);
+        doc.save("posts.pdf");
+      });
+    });
+  };
+
+  const header = (
+    <div className="flex align-items-center export-button">
+      <Button
+        type="button"
+        icon="pi pi-file-excel"
+        onClick={exportExcel}
+        className="p-button-success mr-2"
+        data-pr-tooltip="XLS"
+      />
+      <Button
+        type="button"
+        icon="pi pi-file-pdf"
+        onClick={exportPDF}
+        className="p-button-warning mr-2"
+        data-pr-tooltip="PDF"
+      />
+    </div>
+  );
+
   return (
     <div>
       <DataTable
@@ -20,6 +90,7 @@ const DataTableExample = () => {
         paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
         dataKey="id"
         paginator
+        header={header}
         emptyMessage="No data found."
         className="datatable-responsive"
         currentPageReportTemplate="Showing {first} to {last} of {totalRecords} posts"
